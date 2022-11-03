e13016f9362f8844693593d86dd983c90445199e
jangh <jangh4139@gmail.com>
2022-11-03 20:19:32 +0900

    카드 리스트 작성 완료 카드 딜링 함수 구현 필요

--- POKERGAME.py
+++ POKERGAME.py
@@ -1,3 +1,4 @@
+import random
 #포커카드 리스트 스,다,하,클 순서
 card_type = ["spade", "diamond", "heart", "clover"]
 card_list = []
@@ -5,15 +6,17 @@ spade = []
 diamond = []
 heart = []
 clover = []
-
-for i in card_type:
-    c_type = i
-    for z in (spade, diamond, heart, clover):
-        z.append(c_type + "A")
-        for k in range(2, 11):
-            z.append(c_type + " {0}".format(k))
-        for i in ["Q", "K"]:
-            z.append(c_type + " {0}".format(i))
-        card_list.append(z)
+type_index = 0
+for z in (spade, diamond, heart, clover):
+    z.append(card_type[type_index] + "A")
+    for k in range(2, 11):
+        z.append(card_type[type_index] + " {0}".format(k))
+    for i in ["Q", "K", "J"]:
+        z.append(card_type[type_index] + " {0}".format(i))
+    card_list.append(z)
+    type_index +=1
 print(card_list)
-print(len(card_list), len(card_list[0]) + len(card_list[1]) + len(card_list[2]) + len(card_list[3]))
+print(len(card_list), len(card_list[0]))
+ 
+def Dealing():
+    for i in range
