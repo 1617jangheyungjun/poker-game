8ea80b68d929b6f53b7adf562ae60d3ef721dba8
jangh <jangh4139@gmail.com>
2022-11-09 22:59:01 +0900

    3번쨰 배팅까지 완성

--- POKERGAME.py
+++ POKERGAME.py
@@ -52,7 +52,7 @@ def Dealing(player_count):
     random.shuffle(shuffle_card)
     for i in range(player_count):
         p_c.append([shuffle_card.pop(0), shuffle_card.pop(0+player_count)])
-    for i in range(3):
+    for i in range(4):
         global_card.append(shuffle_card.pop(0)) 
 def betting1(player_count, player_index, indexe):
     for i in range(1):
@@ -63,6 +63,9 @@ def betting1(player_count, player_index, indexe):
         betting_cash[i][0] = betting_cash[i][0]+m_cash
         for p_bet in range(player_count):
             if betting_cash[p_bet][0] == 'Fold':
+                print(f"""{p_bet+1}번째 플레이어는 Fold이기때문에 배팅에 참가할 수 없습니다.
+""")
+                time.sleep(0.5)
                 continue
             if indexe == p_bet:
                 continue
@@ -97,11 +100,64 @@ def betting2(player_count, player_index, indexe):
 """)
     
     for i in range(1):
+        if betting_cash[indexe][0] == 'Fold':
+            print(f"""{indexe+1}번째 플레이어는 Fold이기때문에 배팅에 참가할 수 없습니다.
+""")
+            indexe += 1
+            if indexe > player_count:
+                indexe = 0
+            time.sleep(0.5)
         print(f"{indexe+1}번째 플레이어는 '추가'베팅금액을 결정해주십시오. 현재 가진돈 {cash[indexe][0]}")
         for indexer in range(player_count):
             print(f"{indexer + 1}번째 플레이어의 돈 {cash[indexer][0]} | 배팅금액 : {betting_cash[indexer][0]}")
         m_cash = int(input("추가배팅할 금액 : "))
-        betting_cash[i][0] = betting_cash[i][0]+m_cash
+        betting_cash[indexe][0] = betting_cash[indexe][0]+m_cash
+        for p_bet in range(player_count):
+            if betting_cash[p_bet][0] == 'Fold':
+                continue
+            if indexe == p_bet:
+                continue
+            play_bet = input(f"""{p_bet + 1}번째 플레이어님 콜하시겠습니까? 레이즈하시겠습니까? 폴드하겠습니까?
+                
+콜 = call 레이즈 = raise 폴드 = fold
+영어로 작성해 주십시오 : """)
+            if play_bet == "call":
+                betting_cash[p_bet][0] = betting_cash[p_bet][0]+m_cash
+            elif play_bet == 'fold':
+                fold_player.append(betting_cash[p_bet][0])
+                betting_cash[p_bet][0] = "Fold"
+                fold_player_index.append(p_bet)
+            elif play_bet == "raise":
+                b_raise = int(input(f"얼마를 추가로 베팅하시겠습니까? | 현재 배팅금액 {betting_cash[0][0]} + "))
+                m_cash = m_cash + b_raise
+                raise1(m_cash, b_raise, player_count)
+        indexe += 1
+        if indexe > player_count:
+            indexe = 0
+        betting3(player_count, player_index, indexe)
+
+def betting3(player_count, player_index, indexe):
+    print(f"""
+두번째 배팅이 끝났습니다.
+
+공통카드 3장 및 마지막 카드를 오픈하겠습니다.
+
+{global_card[0], global_card[1], global_card[2]} | 마지막 카드 : {global_card[3]}
+""")
+    
+    for i in range(1):
+        if betting_cash[indexe][0] == 'Fold':
+            print(f"""{indexe+1}번째 플레이어는 Fold이기때문에 배팅에 참가할 수 없습니다.
+""")
+            indexe += 1
+            if indexe > player_count:
+                indexe = 0
+            time.sleep(0.5)
+        print(f"{indexe+1}번째 플레이어는 '추가'베팅금액을 결정해주십시오. 현재 가진돈 {cash[indexe][0]}")
+        for indexer in range(player_count):
+            print(f"{indexer + 1}번째 플레이어의 돈 {cash[indexer][0]} | 배팅금액 : {betting_cash[indexer][0]}")
+        m_cash = int(input("추가배팅할 금액 : "))
+        betting_cash[indexe][0] = betting_cash[indexe][0]+m_cash
         for p_bet in range(player_count):
             if betting_cash[p_bet][0] == 'Fold':
                 continue
@@ -123,10 +179,11 @@ def betting2(player_count, player_index, indexe):
                 raise1(m_cash, b_raise, player_count)
         if player_index == player_count:
             continue
-    
+        
+   
 
-def main():
-    player_index = 0
+
+def main(player_index):
     player_count = int(input("플레이어의 수를 정해주세요 : "))
     print("{0}명의 플레이어로 게임을 시작합니다.".format(player_count))
     print(f"{player_count}명의 플레이어에게 딜링을 하는중입니다.")
@@ -155,5 +212,4 @@ def main():
     
         
     
-main()
-    
+main(0)
